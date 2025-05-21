fixed some bugs in pseudolikelihood calculation. Retrained bert with scheduled mlm_loss

main.py
@@ -303,7 +303,8 @@ class SequentialEBMsTrainer:
                     
                     #_________________test: BERT mlm_loss___________________
                     # print(f'\nxo.shape: {xo.shape}') #4,50
-                    mlm_output, org_loss = self.sebm.forward(xo, None, is_ebm=False)
+                    mlm_output, org_loss = self.sebm.forward(xo, None, is_ebm=False) #xo is partially unmasked 'bert_input'
+                    # print(f'mlm_output.shape: {mlm_output.shape}') #Size(batch_size, |u'|, num_classes)
                     mlm_criterion = nn.CrossEntropyLoss()
                     mlm_loss = mlm_criterion(mlm_output.view(-1, mlm_output.size(-1)), xu.view(-1))
                     # print(f'\nmlm_loss: {mlm_loss}, loss: {org_loss}')
@@ -313,11 +314,11 @@ class SequentialEBMsTrainer:
                     
                     # _____________2-1 tentatively annotated for testing ___________
                     # 2-1. Estimate the 1D conditional logp(xu | xo) via pseudolikelihood
-                    ce_loss = torch.tensor(0., requires_grad=True).to(self.device)
                     contrast_loss = torch.tensor(0., requires_grad=True).to(self.device)
                     # recover the ignored token to padding
                     xu_ = xu.clone()
                     xu_[xu_ == -100] = 0
+                    logp_xu_list, xu_list = [], [] #to store a batch of logp_xu's, of Size(batch_size, |u'|, num_classes)
                     for r in range(xu.size(0)): #iter within batch
                         # print(f'\n_____\nr: {r}, k: {k}\n________\n')
                         # assert torch.nonzero(xu[r]).squeeze().dim(), f"xu[r] is all zero: \n{xu[r]}"
@@ -333,6 +334,8 @@ class SequentialEBMsTrainer:
                         xu_label = xu[r][xu_token_ids]
                         assert logp_xu.size(0) == xu_label.size(0), \
                             f'\nlogp_xu.shape: {logp_xu.shape}, xu_label.shape: {xu_label.shape}'
+                        logp_xu_list.append(logp_xu.unsqueeze(0))
+                        xu_list.append(xu_label.unsqueeze(0))
                         # print(f'logp_xu.shape: {logp_xu.shape}, xu_label.shape({xu_label.shape}): {xu_label}')
                         # if r == 0:
                         #     print(f'xu_token_ids: {xu_token_ids}, \nxu_label: {xu_label}')
@@ -344,24 +347,25 @@ class SequentialEBMsTrainer:
                         #     xo[r], neg_xo[r]
                         # )
                         # #_____________________________
-                        
-                        ce_loss = ce_loss + self.criterion(logp_xu, xu_label)
-                        loss_is_nan = torch.isnan(torch.tensor(ce_loss)).any()
-                        assert loss_is_nan == False, f'\nce_loss becomes NaN! '\
-                            f'\nce_loss: {ce_loss}, is_nan: {loss_is_nan}\n' \
-                            f'logp_xu: \n{logp_xu}, \nxu_label: \n{xu_label}, \ngamma[r]: \n{gamma[r]}'
                     #end of row iter
-                    if logp_xu == None:
-                        break
-                    
+                    batch_logp_xu = torch.cat(logp_xu_list, dim=1).squeeze(0) #flattened: Size(sum of |u'| within the batch, num_classes)
+                    batch_xu = torch.cat(xu_list, dim=1).view(-1) #flattened: Size(sum of |u'| within the batch) 
+                    assert batch_logp_xu.dim(), f'k = {k}, early_stop = {early_stop}, logp_xu_list is empty!: {logp_xu_list}, '
+                    # print(f'\nbatch_logp_xu.shape:{batch_logp_xu.shape}, batch_xu.shape: {batch_xu.shape}')
                     # 2-2. Contrast-loss
-                    # TODO
+                    ce_loss = self.criterion(batch_logp_xu, batch_xu)
+                    loss_is_nan = torch.isnan(ce_loss.clone().detach()).any()
+                    assert loss_is_nan == False, f'\nce_loss becomes NaN! '\
+                        f'\nce_loss: {ce_loss}, is_nan: {loss_is_nan}\n' \
+                        f'batch_logp_xu: \n{batch_logp_xu}, \nbatch_xu: \n{batch_xu}, \ngamma[r]: \n{gamma[r]}'
+                            
                     loss = ce_loss + contrast_loss
                     # loss = mlm_loss
                     if self.train_wandb:
                         wandb.log({"l_ce": ce_loss, "l_contrast": contrast_loss, "loss": org_loss, "mlm_loss": mlm_loss}) #loss: loss
                     # #_________mlm__________
-                    print(f'i: {i}, ce_loss: {ce_loss}, mlm_loss: {mlm_loss}')
+                    if i % 5 == 0:
+                        print(f'i={i}, k={k}, ce_loss: {ce_loss}, mlm_loss: {mlm_loss}')
                     # if i % 100 == 0:
                     #     pred = mlm_output.argmax(dim=-1)
                     #     correct = self.eval_metric(pred, data['bert_label'])
@@ -543,7 +547,7 @@ class SequentialEBMsTrainer:
             # if i % self.log_freq == 0: ###############
             #     data_iter.write(str(post_fix)) ##################
                 
-            break ###############test
+            # break ###############test
             # if i > 500:
             #     break ###############test
         #end of batch iter

playground.py
@@ -1,5 +1,14 @@
 import torch
-print("PyTorch version:", torch.__version__) #2.6.0+cpu
-print("CUDA available:", torch.cuda.is_available()) #False
-if torch.cuda.is_available():
-    print("CUDA version:", torch.version.cuda)
+# print("PyTorch version:", torch.__version__) #2.6.0+cpu
+# print("CUDA available:", torch.cuda.is_available()) #False
+# if torch.cuda.is_available():
+#     print("CUDA version:", torch.version.cuda)
+t1 = torch.tensor([ 7,  8, 20,  7, 10, 20, 11, 20,  9,  7,  1,  2,  2,  2,  2,  2,  2,  2, \
+         2,  2,  2,  2,  2,  2,  2,  2,  2,  3,  0,  0,  0,  0,  0,  0,  0,  0, \
+         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0])
+t2 = torch.tensor([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 27, 28, 29, 30, 31, 32, 33, \
+        34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49])
+result = t1[t2]
+
+print(f'result.shape: {result.shape}')
+print(f'result: {result}')

sequential_ebms.py
@@ -1007,11 +1007,13 @@ class BERTSequentialEBMs():
             f'mlm_label({mlm_label.shape}): {mlm_label}, ' \
             f'mlm_input({mlm_input.shape}): {mlm_input}'
         # print(f'Inside pseudolikelihood: latent.shape: {latent.shape}, mlm_label.shape: {mlm_label.shape}')
-        print(f'\nInside pseudolikelihood(), latent({latent.shape}): \n{latent}\nmlm_label({mlm_label.shape}): \n{mlm_label}\nmlm_input({mlm_input.shape}): \n{mlm_input}')
-        print(f'\nspecial_tok_size: {self.special_tok_size}')
-        u = torch.nonzero(mlm_label != self.tokenizer.pad_tokn_id).squeeze() #Size(|u|)
-        # maintain the input tokens that are not MASK or PAD
-        o = torch.nonzero((mlm_input != self.tokenizer.mask_token_id) and (mlm_input != self.tokenizer.pad_token_id)).squeeze() #include MASK state (the last special token id)
+        # print(f'\nInside pseudolikelihood(), latent({latent.shape})\nmlm_label({mlm_label.shape}): \n{mlm_label}\nmlm_input({mlm_input.shape}): \n{mlm_input}')
+        # 1. generate the unobserved and observed token indices
+        u = torch.nonzero(mlm_label != self.tokenizer.pad_token_id).squeeze() #Size(|u|)
+        # ignore the MASK and PAD tokens in the partially unmasked input
+        o_mask = (mlm_input != self.tokenizer.mask_token_id) & (mlm_input != self.tokenizer.pad_token_id)
+        o = torch.nonzero(o_mask, as_tuple=True)[0]
+        # print(f'after removing padding, o({o.shape}): {o}')
         o_len = o.size(0)
         # u and o can be a single token value
         if u.dim():
@@ -1031,6 +1033,7 @@ class BERTSequentialEBMs():
         if len(u_prime) == 0:
             return None
         assert len(u_prime), f'u_prime is empty!! mlm_label: {mlm_label}, u: {u}, pi: {pi}'
+        # print(f'\nu: {u}, \nu_prime: {u_prime}, \no: {o}')
         for i in range(len(u_prime)): #iter through |u'| EBMs
             condition_vals = mlm_label[u_prime[:i+1], :] #inclusive x_{u'_i} 121??
             condition_latent = latent[u_prime[:i+1], :]
@@ -1038,7 +1041,7 @@ class BERTSequentialEBMs():
             #we found that including MASK state can give better performance? 
             full_vals = torch.cat([mlm_input[o, :], condition_vals], dim=0)
             full_latent = torch.cat([latent[o, :], condition_latent], dim=0)
-            # print(f'\ni: {i}, rest_idx({condition_vals.shape}): \n{condition_vals}\ncondition_latent.shape: {condition_latent.shape}')
+            # print(f'\ni: {i}, full_vals({full_vals.shape}): \n{full_vals.squeeze(-1)}\nfull_latent.shape: {full_latent.shape}')
             
             # ei_dist = self.energy(idx=pi[i], val=False, rest_idx=condition_vals, latent=condition_latent)
             # ei_dist = self.energy(idx=self.inp_len+2+pi[i], val=False, rest_idx=full_vals, latent=full_latent)